4a619c8de312cad59f168e916cc5441236f3687d
nucheale <i.zabolotnyj@ya.ru>
2024-04-04 16:22:04 +0300

    start3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
 from my_database import Database
 from config_data import config
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,9 +14,58 @@ def index():
     return render_template('index.html', h1='Заголовок')
 
 
-@app.route('/register')
+now = datetime.now()
+formatted_datetime = now.strftime('%d.%m.%Y %H:%M')
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', current_date='DATE', current_time='TIME')
+    incorrect_username = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        if db.user_exists(username):
+            incorrect_username = 'Пользователь с таким логином уже существует'
+            # return "user_exists"
+        else:
+            db.add_new_user(username, email, password, now)
+            return redirect('/success_register')
+
+    return render_template('register.html', current_datetime=formatted_datetime, incorrect_username=incorrect_username)
+
+
+# @app.route('/check_username')
+# def check_username():
+#     username = request.form['username']
+#     if db.user_exists(username):
+#         return jsonify({'message': 'Пользователь с таким логином уже существует'})
+#     else:
+#         return jsonify({'message': ''})
+
+
+@app.route('/success_register')
+def success():
+    return 'USER REGISTERED'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    incorrect_data = None
+    if request.method == 'POST':
+        username_email = request.form['username_email']
+        password = request.form['password']
+        if db.user_login(username_email, password):
+            return redirect('/success_login')
+        else:
+            incorrect_data = 'Введена неверная пара логин/пароль'
+
+    return render_template('login.html', current_datetime=formatted_datetime, incorrect_data=incorrect_data)
+
+
+@app.route('/success_login')
+def success_login():
+    return 'success_login'
 
 
 if __name__ == '__main__':
--- my_database.py
+++ my_database.py
@@ -1,6 +1,7 @@
 import sqlite3
-
-
+# from datetime import datetime
+#
+# now = datetime.now()
 class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file, check_same_thread=False)
@@ -8,7 +9,35 @@ class Database:
 
     def create_tables(self):
         with self.connection:
-            self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, time TEXT)')
+            self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                                ' username TEXT, email TEXT, password TEXT, time TEXT)')
+            return
 
+    def user_exists(self, username):
+        with self.connection:
+            result = self.cursor.execute(f"SELECT id FROM users WHERE username = '{username}'").fetchone()
+            if not result:
+                return False
+            else:
+                return True
 
-    # def new_user(self, username, password):
+    def add_new_user(self, username, email, password, timestamp):
+        with self.connection:
+            self.cursor.execute(f"INSERT INTO users (username, email, password, time) VALUES ('{username}', '{email}', '{password}', '{timestamp}')")
+            return
+
+    def user_login(self, username_email, password):
+        with self.connection:
+            success = False
+            login_username = self.cursor.execute(f"SELECT id FROM users WHERE username = '{username_email}' AND password = '{password}'").fetchone()
+            if login_username:
+                success = True
+                return success
+            else:
+                login_email = self.cursor.execute(f"SELECT id FROM users WHERE email = '{username_email}' AND password = '{password}'").fetchone()
+                if login_email:
+                    success = True
+                    return success
+                else:
+                    print(success)
+                    return success
